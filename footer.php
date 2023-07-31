1eec2467e0dac12a2488a5e085bec7ada958a879
RadonantenainaRodolphe <65818881+RadonantenainaRodolphe@users.noreply.github.com>
2023-08-01 00:12:00 +0200

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,11 @@
+ <!-- Footer -->
+ <div class="mt-5 py-3" style="background-color : #2596be;">
+    <div class="d-flex justify-content-between">
+        <div class="align-self-center">
+            <span class="text-white font-weight-bold pl-4">ReserVeo</span>
+        </div>
+        <div class="align-self-center">
+            <span class="text-white font-weight-bold align-self-center">&#169 2023 </span>developed and maintained by <a href="https://www.facebook.com/radonantenaina.rodolph" target="_blank" class="text-white font-weight-bold pe-4">Radonantenaina Rodolph</a>
+        </div>
+    </div>
+</div>
